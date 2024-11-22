abbf55e6e20299cc4289ebba1ed79c8432819988
Kenneth Dombrowski <kenneth@ylayali.net>
2024-11-22 09:56:57 -0500

    bidirectional communication

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import asyncio
+import json
 
 from websockets.asyncio.server import serve
 from websockets.exceptions import ConnectionClosedOK
@@ -8,6 +9,35 @@ from websockets.exceptions import ConnectionClosedOK
 async def handler(websocket):
     async for message in websocket:
         print(message)
+import json
+
+from connect4 import PLAYER1, PLAYER2
+
+async def handler(websocket):
+    for player, column, row in [
+        (PLAYER1, 3, 0),
+        (PLAYER2, 3, 1),
+        (PLAYER1, 4, 0),
+        (PLAYER2, 4, 1),
+        (PLAYER1, 2, 0),
+        (PLAYER2, 1, 0),
+        (PLAYER1, 5, 0),
+    ]:
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+        await asyncio.sleep(0.5)
+
+    event = {
+        "type": "win",
+        "player": PLAYER1,
+    }
+    await websocket.send(json.dumps(event))
+
 
 async def main():
     async with serve(handler, "", 8001):
