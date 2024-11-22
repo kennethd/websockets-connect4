88d21250ad64cf9908ab805a22fad1187797fa0f
Kenneth Dombrowski <kenneth@ylayali.net>
2024-11-22 10:45:46 -0500

    forgot to remove old handler()

--- app.py
+++ app.py
@@ -26,58 +26,38 @@ async def handler(websocket):
         move = move + 1
         player = players[move % 2]
 
-        response = {}
-        if event.get("type") == "play":
-            column = event.get("column")
+        event_type = event.get("type")
+        response = {
+            "type": "error",
+            "message": f"Unknown event type {event_type}",
+        }
+
+        if event_type == "play":
             try:
+                column = event["column"]
                 row = game.play(player, column)
-
-                if game.last_player_won:
-                    response = {
-                        "type": "win",
-                        "player": player,
-                    }
-                else:
-                    response = {
-                        "type": "play",
-                        "player": player,
-                        "column": column,
-                        "row": row,
-                    }
-
-            except ValueError as exc:
+            except (ValueError, KeyError) as exc:
                 response = {
                     "type": "error",
                     "message": str(exc),
                 }
+                await websocket.send(json.dumps(response))
+                continue
 
-        await websocket.send(json.dumps(event))
+            if game.last_player_won:
+                response = {
+                    "type": "win",
+                    "player": player,
+                }
+            else:
+                response = {
+                    "type": "play",
+                    "player": player,
+                    "column": column,
+                    "row": row,
+                }
 
-
-async def handler(websocket):
-    for player, column, row in [
-        (PLAYER1, 3, 0),
-        (PLAYER2, 3, 1),
-        (PLAYER1, 4, 0),
-        (PLAYER2, 4, 1),
-        (PLAYER1, 2, 0),
-        (PLAYER2, 1, 0),
-        (PLAYER1, 5, 0),
-    ]:
-        event = {
-            "type": "play",
-            "player": player,
-            "column": column,
-            "row": row,
-        }
-        await websocket.send(json.dumps(event))
-        await asyncio.sleep(0.5)
-
-    event = {
-        "type": "win",
-        "player": PLAYER1,
-    }
-    await websocket.send(json.dumps(event))
+        await websocket.send(json.dumps(response))
 
 
 async def main():
