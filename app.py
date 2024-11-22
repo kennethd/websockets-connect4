95979cb25c12aa819c0f9d82efff1281bc8a3b69
Kenneth Dombrowski <kenneth@ylayali.net>
2024-11-22 10:33:24 -0500

    basic game logic -- server alternates between users

--- app.py
+++ app.py
@@ -2,16 +2,57 @@
 
 import asyncio
 import json
+import logging
 
 from websockets.asyncio.server import serve
 from websockets.exceptions import ConnectionClosedOK
 
-async def handler(websocket):
-    async for message in websocket:
-        print(message)
-import json
+from connect4 import PLAYER1, PLAYER2, Connect4
 
-from connect4 import PLAYER1, PLAYER2
+
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+
+async def handler(websocket):
+    game = Connect4()
+    move = 0
+    players = [PLAYER1, PLAYER2]
+
+    async for message in websocket:
+        logging.debug(message)
+        event = json.loads(message)
+        logging.debug(event)
+        # handle play events, alternating between users
+        move = move + 1
+        player = players[move % 2]
+
+        response = {}
+        if event.get("type") == "play":
+            column = event.get("column")
+            try:
+                row = game.play(player, column)
+
+                if game.last_player_won:
+                    response = {
+                        "type": "win",
+                        "player": player,
+                    }
+                else:
+                    response = {
+                        "type": "play",
+                        "player": player,
+                        "column": column,
+                        "row": row,
+                    }
+
+            except ValueError as exc:
+                response = {
+                    "type": "error",
+                    "message": str(exc),
+                }
+
+        await websocket.send(json.dumps(event))
+
 
 async def handler(websocket):
     for player, column, row in [
